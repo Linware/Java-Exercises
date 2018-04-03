913dc01e2acf5cda78d72eca98f0be0832b7209e
Jonan Oribe <disaicold@gmail.com>
2018-04-03 17:29:16 +0200

    Basics

--- JUnit_Test/src/JUnit_Test/ClaseTest.java
+++ JUnit_Test/src/JUnit_Test/ClaseTest.java
@@ -1,5 +1,7 @@
 package JUnit_Test;
 
+import java.util.Arrays;
+
 public class ClaseTest {
 	
 	public ClaseTest(){};
@@ -8,5 +10,27 @@ public class ClaseTest {
 	}
 	public String addString(String c,String e) {
 		return c+e;
+	}
+	
+	public static long subtract(long ... operands) {
+		long ret=operands[0];
+		for(int aa=1;aa<operands.length;aa++) {
+			ret -= operands[aa];
+		}
+		return ret;
+	}
+	
+	public static long add(long ... operands) {
+		long ret=operands[0];
+		for(int aa=1;aa<operands.length;aa++) {
+			ret+=operands[aa];
+		}
+		return ret;
+	}
+	
+	public static int[] ClaseAssertArrayEquals(int[] expected) {
+		Arrays.sort(expected);
+		return expected;
+		
 	}
 }
--- JUnit_Test/src/JUnit_Test/ClaseTestTest.java
+++ JUnit_Test/src/JUnit_Test/ClaseTestTest.java
@@ -2,11 +2,15 @@ package JUnit_Test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 public class ClaseTestTest {
 
@@ -14,6 +18,10 @@ public class ClaseTestTest {
 	public static void setUpBeforeClass() throws Exception {
 
 	}
+	
+    @Before public void initialize() {
+		ClaseTest test= new ClaseTest();
+     }
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
@@ -35,7 +43,7 @@ public class ClaseTestTest {
 	
 	@Test
 	public void TestAddNumber() {
-		ClaseTest test= new ClaseTest();
+
 		int a=9;
 		int b=11;
 		
@@ -47,12 +55,42 @@ public class ClaseTestTest {
 	@Test
 	public void TestAddString() {
 		ClaseTest test= new ClaseTest();
+
 		String c="Hello ";
 		String e="World!";
 		
-		String result=test.addString(c,e);
+		String result=test.addString(c, e);
+
 		assertEquals("Hello World!",result);
 		
 	}
+	
+	@Test
+	public void testSubtract() {
+		long result= 100-12-(-6)-4;
+		assertEquals(result,ClaseTest.subtract(100,12,-6,4));
+		result=0;
+		assertEquals(result,ClaseTest.subtract(0,0,0,0));
+	}
+	
+	@Test
+	public void testAdd() {
+		long result= 3+4+5+2;
+		assertEquals(result,ClaseTest.add(3,4,5,2));
+		result=12;
+		assertEquals(result,ClaseTest.add(5,6,1));
+	}
+	
+	@Test
+	public void testClaseAssertArrayEquals() {
+
+		int[] numbers= {1,9,5,8,7};
+		int[] expected= {1,5,7,8,9};
+		ClaseTest.ClaseAssertArrayEquals(numbers);
+		
+		assertArrayEquals(numbers,expected);
+	}
+
+
 
 }
